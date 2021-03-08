c4966386756a538b2b03beadf3041d160a5133d0
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-03-08 01:17:31 +0100

    Ajout des controllers modesolo ( recupàre les questions a l'api - ajout controller multi (vide) - ControllerMode : reoriente maintenant vers la pages demarrage solo ou attente multi (non implemente sur cette branche) - raf , a gerer les erreurs et a les renvoye au cliuent si la requete ajax echoue (die mit en place es ce bon ??

---
+++ Controller/modeSoloController.php
@@ -0,0 +1,56 @@
+<?php
+
+    $adresseDistante ="https://quizz.servehttp.com/";
+    $adresseLocal ="http://127.0.0.1:6969/";
+    $route ="questions/modesolo";
+
+    // recupérer le mode du client :
+    // format { "mode" : valueInt }
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    @$modeActif = $request->mode;
+
+    // si on est en mode solo (1) alors on 
+    if($modeActif == 1){
+
+        // interrogation de l'api en mode solo:
+
+
+        $adresse = $adresseLocal . $route;
+
+        // ************* Call API en get:
+        
+        // create & initialize a curl session
+        $ch = curl_init();
+        
+        // set our url with curl_setopt()
+        curl_setopt($ch, CURLOPT_URL, $adresse );
+        
+        
+        //decommenter la ligne pour le post:
+        // curl_setopt($ch, CURLOPT_POST, 1);// set post data to true
+        
+        // return the transfer as a string, also with setopt()
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        // recup json result
+        $json = curl_exec($ch);
+
+        // close http caller
+        curl_close ($ch);
+
+        // convert json string to an object (array or simple object)
+        $questions = json_decode($json);
+
+        // retour des questions au client :
+        $arr = array('chemin' => 'demarrage solo');
+        //print_r(json_encode($arr));
+        print_r(json_encode($questions));
+
+    }
+    else{    // si on est en mode multi (2) alors on redirige vers le serveur (controller) websocket 
+        // qui s'occupe du reste.
+        die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
+    }
+
+?>
--- Controller/receptionModeController.php
+++ Controller/receptionModeController.php
@@ -1,6 +1,6 @@
+
 <?php
     // recupérer le mode du client :
-    // format { "mode" : valueInt }
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     @$modeActif = $request->mode;
@@ -8,44 +8,13 @@
     // si on est en mode solo (1) alors on 
     if($modeActif == 1){
 
-        // interrogation de l'api en mode solo:
-
-        //todo : changer l'url
-        $adresseDistante ="https://quizz.servehttp.com/questions/getall";
-
-        // ************* Call API en get:
-        
-        // create & initialize a curl session
-        $ch = curl_init();
-        
-        // set our url with curl_setopt()
-        curl_setopt($ch, CURLOPT_URL, $adresseDistante );
-        
-        
-        //decommenter la ligne pour le post:
-        // curl_setopt($ch, CURLOPT_POST, 1);// set post data to true
-        
-        // return the transfer as a string, also with setopt()
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
-        // recup json result
-        $json = curl_exec($ch);
-
-        // close http caller
-        curl_close ($ch);
-
-        // convert json string to an object (array or simple object)
-        $questions = json_decode($json);
-
-        // retour des questions au client :
-        $arr = array('chemin' => 'demarrage solo');
-        //print_r(json_encode($arr));
-        print_r(json_encode($questions));
+        $arr = array('chemin' => 'demarrage-solo');
+        print_r(json_encode($arr));
 
     }
     else{    // si on est en mode multi (2) alors on redirige vers le serveur (controller) websocket 
         // qui s'occupe du reste.
-        $arr = array('chemin' => 'salle attente');
+        $arr = array('chemin' => 'salle-attente');
         print_r(json_encode($arr));
     }
 
